62745735199d6622a2ef35ea4ebe07a351d496ac
SamuelMTL <95709293+SamuelMTL@users.noreply.github.com>
2024-06-04 15:58:36 -0300

    começando a funcionar

--- Mini05_Watch Watch App/View/Screens/Home/HomeButton.swift
+++ Mini05_Watch Watch App/View/Screens/Home/HomeButton.swift
@@ -14,13 +14,13 @@ struct HomeButton<Destination: View>: View {
     var body: some View {
         NavigationLink(destination: destination){
             ZStack{
-                RoundedRectangle(cornerSize: CGSize(width: 20, height: 20))
+                RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
                     .rotation(.degrees(45))
-                    .frame(width: 100,height: 100)
-                    .foregroundStyle(.cyan)
+                    .frame(width: 70,height: 70)
+                    .foregroundStyle(.gray)
                 Text(name)
                     .foregroundStyle(.black)
-                    .padding(15)
+                    .font(.caption)
                 
                 
             }
--- Mini05_Watch Watch App/View/Screens/Home/HomeButtonView.swift
+++ Mini05_Watch Watch App/View/Screens/Home/HomeButtonView.swift
@@ -12,43 +12,59 @@ struct HomeView: View {
     @Environment(\.modelContext) var context
     @Query(sort: \RunData.date) var runData: [RunData]
     
+    private let buttons: [HomeButtonData] = [
+        HomeButtonData(id: 0, name: "Iniciar", destination: AnyView(SelectEvaluationView())),
+        HomeButtonData(id: 1, name: "Avaliações", destination: AnyView(GraphView())),
+        HomeButtonData(id: 2, name: "Conquistas", destination: AnyView(AchievementView()))
+    ]
+    
     var body: some View {
         NavigationStack{
-            List{
-                
-                HomeButton(name: "Avaliação", destination: SelectEvaluationView())
-
-                    .padding(20)
-                HomeButton(name: "Flexao", destination: PUView(viewModel: PUViewModel(model: PUModel())))
-                    .padding(20)
-                HomeButton(name: "Gráficos", destination: GraphView())
-                    .padding(20)
-                HomeButton(name: "Conquistas", destination: AchievementView())
-                    .padding(20)
-
-                
-                Button("Save data") {
-                    let data1 = RunData(date: Date(), totalTime: 90, totalDistance: 10, totalEnergy: 100, avgHeartRate: 110, avgSpeed: 8)
-                    let data2 = RunData(date: Date(timeIntervalSinceNow: -60*60*24), totalTime: 120, totalDistance: 12, totalEnergy: 120, avgHeartRate: 110, avgSpeed: 9)
-                    let data3 = RunData(date: Date(timeIntervalSinceNow: 60*60*24*2), totalTime: 180 ,totalDistance: 9, totalEnergy: 90, avgHeartRate: 105, avgSpeed: 10)
+            VStack{
+                Text("Ascender")
+                    .multilineTextAlignment(.leading)
+                Text("Iniciar uma nova avaliação")
+                    .multilineTextAlignment(.leading)
+                Spacer()
+                ScrollView(.horizontal, showsIndicators: false){
+                    LazyHStack{
+                        Rectangle()
+                            .frame(width: 25)
+                            .foregroundStyle(.clear)
+                        ForEach(buttons, id: \.name) { button in
+                            HomeButton(name: button.name, destination: button.destination)
+                                .scrollTransition(axis: .horizontal) {
+                                    content, phase in
+                                    content.scaleEffect(x: phase.isIdentity ? 1 : 0.4,
+                                                        y: phase.isIdentity ? 1 : 0.4)
+                                }
+                                .padding(10)
+                            
+                        }
+                    }
                     
-                    context.insert(data1)
-                    context.insert(data2)
-                    context.insert(data3)
-                    
-                    print(runData.description)
-                }
-                
-                Button("Delete all data") {
-                    context.container.deleteAllData()
-                    
-                    print(runData.description)
+                    //                Button("Save data") {
+                    //                    let data1 = RunData(date: Date(), totalTime: 90, totalDistance: 10, totalEnergy: 100, avgHeartRate: 110, avgSpeed: 8)
+                    //                    let data2 = RunData(date: Date(timeIntervalSinceNow: -60*60*24), totalTime: 120, totalDistance: 12, totalEnergy: 120, avgHeartRate: 110, avgSpeed: 9)
+                    //                    let data3 = RunData(date: Date(timeIntervalSinceNow: 60*60*24*2), totalTime: 180 ,totalDistance: 9, totalEnergy: 90, avgHeartRate: 105, avgSpeed: 10)
+                    //
+                    //                    context.insert(data1)
+                    //                    context.insert(data2)
+                    //                    context.insert(data3)
+                    //
+                    //                    print(runData.description)
+                    //                }
+                    //
+                    //                Button("Delete all data") {
+                    //                    context.container.deleteAllData()
+                    //
+                    //                    print(runData.description)
+                    //                }
                 }
             }
-            .listStyle(.carousel)
-            .navigationBarBackButtonHidden()
         }
     }
+    
 }
 
 #Preview {
@@ -56,3 +72,8 @@ struct HomeView: View {
         .environmentObject(HealthKitManager())
 }
 
+struct HomeButtonData: Identifiable {
+    let id: Int
+    let name: String
+    let destination: AnyView
+}
