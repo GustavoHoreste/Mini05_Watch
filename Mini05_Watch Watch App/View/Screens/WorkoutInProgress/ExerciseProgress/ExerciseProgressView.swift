464521b25451c904d1184b1568313aaa026c2580
GustavoHoreste <101297032+GustavoHoreste@users.noreply.github.com>
2024-06-06 17:44:12 -0300

    sera que vai?

--- Mini05_Watch Watch App/View/Screens/WorkoutInProgress/ExerciseProgress/ExerciseProgressView.swift
+++ Mini05_Watch Watch App/View/Screens/WorkoutInProgress/ExerciseProgress/ExerciseProgressView.swift
@@ -31,7 +31,7 @@ struct ExerciseProgressView: View {
                                                  extensionName: "")
                     }
                 default:
-                    EmptyView()
+//                    EmptyView()
                     let _ = print("view vazia")
                 }
             }
